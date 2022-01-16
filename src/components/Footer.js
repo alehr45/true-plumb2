9e50eb9f0f9a8507c5dbd932e1aa9c1c87a80b79
Alex Lehr <alexsmacbook@Alexs-MacBook-Air.local>
2022-01-16 14:25:19 -0600

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Footer() {
+    return (
+        <footer>
+
+            <small>TruePlumb - Copyright 2022</small>
+        </footer>
+    )
+}
+
+
+export default Footer
---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+function Home() {
+
+    return (
+        <>
+            <div>
+                <div>
+                    <h1>Welcome to True Plumb 2.0</h1>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home;
